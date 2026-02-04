caf489021892b33570454f8d7d3cdc4dbe349845
allawi011 <ali2026allawi@gmail.com>
2026-02-04 18:51:19 +0300

    Update SplashActivity.kt

--- app/src/main/java/io/kodular/allawi/getmoney/ui/SplashActivity.kt
+++ app/src/main/java/io/kodular/allawi/getmoney/ui/SplashActivity.kt
@@ -1,1 +1,26 @@
 
+package io.kodular.allawi.getmoney.ui
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val user = FirebaseAuth.getInstance().currentUser
+
+        if (user != null) {
+            // مسجل دخول
+            startActivity(Intent(this, MainActivity::class.java))
+        } else {
+            // مو مسجل
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        finish()
+    }
+}
